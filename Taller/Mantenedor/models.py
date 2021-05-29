8cf8a7eae111ed7cac10499d1e84b4e44845838f
Marfullsen <marfullsen@gmail.com>
2021-05-29 15:08:41 -0400

    Modelo de creación de formulario agregado

--- Taller/Mantenedor/models.py
+++ Taller/Mantenedor/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 from django.db.models.fields import BLANK_CHOICE_DASH
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+class FormularioRegistro(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False)
+    last_name = forms.CharField(max_length=30, required=False)
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = User
+        fields = ('email', 'username', 'first_name', 'last_name')
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.username = '{}.{}'.format(
+            self.cleaned_data['last_name'],
+            self.cleaned_data['first_name']
+        )
+        if commit:
+            user.save()
+        return user
 class AuthGroup(models.Model):
     name = models.CharField(unique=True, max_length=150, blank=True, null=True)
 
