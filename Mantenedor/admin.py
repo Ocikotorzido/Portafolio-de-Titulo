f9cc60dea43b3442aa3e6b65eda3d0faf286a46b
Marfullsen <marfullsen@gmail.com>
2021-06-05 16:21:43 -0400

    Models agregados al panel de Administración

--- Mantenedor/admin.py
+++ Mantenedor/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(Perfil)
+admin.site.register(Empleado)
+admin.site.register(Proveedor)
+admin.site.register(Cliente)
