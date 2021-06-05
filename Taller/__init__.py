9a96a1f57824cfb1befb9f5264fa6a9684832a1f
Marfullsen <marfullsen@gmail.com>
2021-06-04 21:56:00 -0400

    Compatibilidad con PC Lorca

--- Taller/__init__.py
+++ Taller/__init__.py
@@ -1,2 +1,7 @@
-import cx_Oracle
-cx_Oracle.init_oracle_client(lib_dir= r"C:\app\JackStrick\product\18.0.0\dbhomeXE\bin")
+try: 
+  import cx_Oracle
+  cx_Oracle.init_oracle_client()
+except cx_Oracle.DatabaseError:
+  import cx_Oracle
+  try: cx_Oracle.init_oracle_client(lib_dir= r"C:\app\JackStrick\product\18.0.0\dbhomeXE\bin")
+  except cx_Oracle.DatabaseError: cx_Oracle.init_oracle_client(lib_dir= r"C:\app\%USERNAME%\product\18.0.0\dbhomeXE\bin")
