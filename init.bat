93963334d29512c0df39e51687b6285fda1a16c1
Marfullsen <marfullsen@gmail.com>
2021-06-19 15:41:00 -0400

    Inicializador de atajos agregado.

---
+++ init.bat
@@ -0,0 +1,7 @@
+@echo off
+doskey rs=python manage.py runserver
+doskey ps=python manage.py shell
+doskey mm=python manage.py makemigrations
+doskey m=python manage.py migrate
+doskey dbs=python manage.py dbshell
+doskey idb=python manage.py inspectdb
